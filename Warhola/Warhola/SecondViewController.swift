8fa9ee54e387cae75324d17d2c76ce1896e6631e
Kagan <bkaganyildiz@github.com>
2017-02-12 13:59:56 +0200

    Initial commits

---
+++ Warhola/Warhola/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Warhola
+//
+//  Created by Kagan YILDIZ on 12/02/17.
+//  Copyright © 2017 Kagan YILDIZ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

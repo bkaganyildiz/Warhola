05bc2cf4267e0d5267a0a4c33ef85d9bde13d16d
Kagan <bkaganyildiz@github.com>
2017-02-12 14:45:57 +0200

    Google Maps SDK has been added

--- Warhola/Warhola/FirstViewController.swift
+++ Warhola/Warhola/FirstViewController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import GoogleMaps
+import MapKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view =  mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Ankara"
+        marker.snippet = "Turkey"
+        marker.map = mapView
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
